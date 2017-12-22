54a3ae92f612a5762a4d53c789d41a1705fa9749
Gabriele <gabriele.nava@mail.polimi.it>
2017-12-22 14:16:17 +0100

    added iCubGenova04 config files

---
+++ controllers/torqueWalking/app/robots/iCubGenova04/initStateMachineExample.m
@@ -0,0 +1,132 @@
+% INITSTATEMACHINEEXAMPLE initializes the robot configuration for running
+%                         'EXAMPLE_STATE_MACHINE' demo. 
+%
+% USAGE: please note that this function is automatically executed when
+%        running the Simulink model.
+%
+% Authors: Daniele Pucci, Marie Charbonneau, Gabriele Nava
+%          
+%          all authors are with the Italian Istitute of Technology (IIT)
+%          email: name.surname@iit.it
+%
+% Genoa, Dec 2017
+%
+
+%% --- Initialization ---
+
+% OVERWRITE SOME CONFIGURATION PARAMETERS FOR RUNNING THE EXAMPLE_STATE_MACHINE DEMO
+if strcmp(DEMO_TYPE,'EXAMPLE_STATE_MACHINE')
+    
+    % Joint torque saturation
+    Sat.tau_max = 60; % [Nm]
+
+    % Saturation on torque derivative (for QP solver)
+    Sat.tauDot_max = 10000;
+
+    % Saturation on state jerk (for QP based inverse kinematics)
+    Sat.nuDDot_max = 10000;
+
+    % Weight for the joint minimization task
+    Sat.weight_tau = 0.01;
+
+    % Damping for the pseudoinverse used for computing the floating base velocity
+    Sat.pinvDamp_nu_b = 1e-6;
+    
+    % True if left foot is initially in contact with the ground (if false,
+    % right foot is assumed to be in contact)
+    Config.LFoot_in_contact_at0 = true;
+
+    %% Smoothing of reference trajectories
+
+    % If true, reference trajectories are smoothed internally
+    Config.SMOOTH_COM_REF      = true;
+    Config.SMOOTH_LFOOT_POS    = true;
+    Config.SMOOTH_RFOOT_POS    = true;
+    Config.SMOOTH_LFOOT_ORIENT = true; 
+    Config.SMOOTH_RFOOT_ORIENT = true; 
+    Config.SMOOTH_ROT_TASK_REF = true;
+    Config.SMOOTH_JOINT_REF    = true; 
+
+    % Smoothing time for tasks and joints references [s]
+    Config.smoothingTime_CoM    = [2;2;2;2;2;2;2];
+    Config.smoothingTime_LFoot  = [2;2;2;2;2;2;2];
+    Config.smoothingTime_RFoot  = [2;2;2;2;2;2;2];
+    Config.smoothingTime_joints = [2;2;2;2;2;2;2];
+
+    % Gains that will influence the smoothing of reference orientations. The
+    % higher, the faster. Only positive or null values.
+    Config.LFoot_Kp_smoothing    = [1;1;1;1;1;1;1];
+    Config.LFoot_Kd_smoothing    = [1;1;1;1;1;1;1];
+    Config.RFoot_Kp_smoothing    = [1;1;1;1;1;1;1];
+    Config.RFoot_Kd_smoothing    = [1;1;1;1;1;1;1];
+    Config.rot_task_Kp_smoothing = [1;1;1;1;1;1;1];
+    Config.rot_task_Kd_smoothing = [1;1;1;1;1;1;1];
+
+    % Smoothing time for gain scheduling [s].
+    Config.smoothingTimeGains    = [1;1;1;1;1;1;1];
+   
+    %% Gains matrices
+
+    % CoM position and velocity gains
+    Gains.Kp_CoM = [50, 50, 50; ...  % state = 1 two feet balancing
+                    50, 50, 50; ...  % state = 2 move CoM on left foot
+                    50, 50, 50; ...  % state = 3 left foot balancing
+                    50, 50, 50; ...  % state = 4 move right foot origin
+                    50, 50, 50; ...  % state = 5 move right foot orientation
+                    50, 50, 50; ...  % state = 6 move right foot back
+                    50, 50, 50];     % state = 7 two feet balancing
+                
+    Gains.Kd_CoM = 2*sqrt(Gains.Kp_CoM);
+
+    % Feet position and velocity gains
+    Gains.Kp_LFoot =   [50, 50, 50, 30, 30, 30; ...  % state = 1 two feet balancing
+                        50, 50, 50, 30, 30, 30; ...  % state = 2 move CoM on left foot
+                        50, 50, 50, 30, 30, 30; ...  % state = 3 left foot balancing
+                        50, 50, 50, 30, 30, 30; ...  % state = 4 move right foot origin
+                        50, 50, 50, 30, 30, 30; ...  % state = 5 move right foot orientation
+                        50, 50, 50, 30, 30, 30; ...  % state = 6 move right foot back
+                        50, 50, 50, 30, 30, 30];     % state = 7 two feet balancing
+              
+    Gains.Kd_LFoot = 2*sqrt(Gains.Kp_LFoot);
+
+    Gains.Kp_RFoot =   [50, 50, 50, 30, 30, 30; ...  % state = 1 two feet balancing
+                        50, 50, 50, 30, 30, 30; ...  % state = 2 move CoM on left foot
+                        50, 50, 50, 30, 30, 30; ...  % state = 3 left foot balancing
+                        50, 50, 50, 30, 30, 30; ...  % state = 4 move right foot origin
+                        50, 50, 50, 30, 30, 30; ...  % state = 5 move right foot orientation
+                        50, 50, 50, 30, 30, 30; ...  % state = 6 move right foot back
+                        50, 50, 50, 30, 30, 30];     % state = 7 two feet balancing
+
+    Gains.Kd_RFoot = 2*sqrt(Gains.Kp_RFoot); 
+
+    % Root link orientation and angular velocity gains
+    Gains.Kp_rot_task = [20, 20, 20; ...  % state = 1 two feet balancing
+                         20, 20, 20; ...  % state = 2 move CoM on left foot
+                         20, 20, 20; ...  % state = 3 left foot balancing
+                         20, 20, 20; ...  % state = 4 move right foot origin
+                         20, 20, 20; ...  % state = 5 move right foot orientation
+                         20, 20, 20; ...  % state = 6 move right foot back
+                         20, 20, 20];     % state = 7 two feet balancing
+                 
+    Gains.Kd_rot_task =  2*sqrt(Gains.Kp_rot_task); 
+
+    % Joint position and velocity gains
+    
+                        % torso      % left arm      % right arm     % left leg               % right leg                                   
+    Gains.impedances = [20  20  20,  10  10  10  8,  10  10  10  8,  30  30  30  60  10  10,  30  30  30  60  10  10;  ... % state = 1 two feet balancing          
+                        20  20  20,  10  10  10  8,  10  10  10  8,  30  30  30  60  10  10,  30  30  30  60  10  10;  ... % state = 2 move CoM on left foot
+                        20  20  20,  10  10  10  8,  10  10  10  8,  30  30  30  60  10  10,  30  30  30  60  10  10;  ... % state = 3 left foot balancing
+                        20  20  20,  10  10  10  8,  10  10  10  8,  30  30  30  60  10  10,  30  30  30  60  10  10;  ... % state = 4 move right foot origin        
+                        20  20  20,  10  10  10  8,  10  10  10  8,  30  30  30  60  10  10,  30  30  30  60  10  10;  ... % state = 5 move right foot orientation
+                        20  20  20,  10  10  10  8,  10  10  10  8,  30  30  30  60  10  10,  30  30  30  60  10  10;  ... % state = 6 move right foot back
+                        20  20  20,  10  10  10  8,  10  10  10  8,  30  30  30  60  10  10,  30  30  30  60  10  10]; ... % state = 7 two feet balancing
+    
+    Gains.dampings   = zeros(size(Gains.impedances));
+
+    %% Constraints for QP for balancing - friction cone - z-moment - in terms of f
+    forceFrictionCoefficient     = 1;  
+    torsionalFrictionCoefficient = 1/75;
+    
+end
+
+    
