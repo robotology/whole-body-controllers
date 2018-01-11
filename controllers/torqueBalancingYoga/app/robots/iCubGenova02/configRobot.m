e8079bb9ceec91a941b3b39d0b7e4d54abb65a1e
Gabriele <gabriele.nava@mail.polimi.it>
2018-01-11 11:07:47 +0100

    added icubGenova02 for all controllers

--- controllers/torqueBalancingYoga/app/robots/iCubGenova02/configRobot.m
+++ controllers/torqueBalancingYoga/app/robots/iCubGenova02/configRobot.m
@@ -3,11 +3,11 @@
 %
 
 %% --- Initialization ---
-Config.ON_GAZEBO         = true;
+Config.ON_GAZEBO         = false;
 ROBOT_DOF                = 23;
 ROBOT_DOF_FOR_SIMULINK   = eye(ROBOT_DOF);
 
-WBT_robotName            = 'icubSim';
+WBT_robotName            = 'icub';
 WBT_wbiList              = '(torso_pitch,torso_roll,torso_yaw,l_shoulder_pitch, l_shoulder_roll, l_shoulder_yaw, l_elbow, r_shoulder_pitch,r_shoulder_roll, r_shoulder_yaw, r_elbow, l_hip_pitch, l_hip_roll, l_hip_yaw, l_knee, l_ankle_pitch, l_ankle_roll, r_hip_pitch,r_hip_roll,r_hip_yaw,r_knee,r_ankle_pitch,r_ankle_roll)';
 
 % Frames list
