a8e61e8a0c8f6fbd8ec6c58a7c2990996beb35eb
Gabriele <gabriele.nava@iit.it>
2018-12-27 10:32:37 +0100

    fixed fromTransfMatrixToPosQuat.m

--- library/matlab-wbc/+wbc/fromTransfMatrixToPosQuat.m
+++ library/matlab-wbc/+wbc/fromTransfMatrixToPosQuat.m
@@ -21,28 +21,68 @@ function q = fromTransfMatrixToPosQuat(H)
     %% --- Initialization ---
 
     % Separate the rotation matrix
-    R = H(1:3,1:3);
+    R       = H(1:3,1:3);
+    qt_b    = zeros(4,1);
+    
+    % min. value to treat a number as zero
+    epsilon = 1e-12; 
+    
+    % Compute the corresponding (unit) quaternion from a given rotation matrix R:
+    %
+    % The transformation uses the computational efficient algorithm of Stanley.
+    % To be numerically robust, the code determines the set with the maximum
+    % divisor for the calculation.
+    %
+    % For further details about the Stanley Algorithm, see:
+    %   [1] Optimal Spacecraft Rotational Maneuvers, John L. Junkins & James D. Turner, Elsevier, 1986, pp. 28-29, eq. (2.57)-(2.59).
+    %   [2] Theory of Applied Robotics: Kinematics, Dynamics, and Control, Reza N. Jazar, 2nd Edition, Springer, 2010, p. 110, eq. (3.149)-(3.152).
+    % Note: There exist also an optimized version of the Stanley method and is the fastest
+    %       possible computation method for Matlab, but it does not cover all special cases.
+    % Further details about the fast calculation can be found at:
+    %   [3] Modelling and Control of Robot Manipulators, L. Sciavicco & B. Siciliano, 2nd Edition, Springer, 2008,
+    %       p. 36, formula (2.30).
+    %
+    tr = R(1,1) + R(2,2) + R(3,3);
+    
+    if (tr > epsilon)
+        
+            % scalar part:
+            qt_b(1,1) = 0.5*sqrt(tr + 1);
+            s_inv     = 1/(qt_b(1,1)*4);
+            
+            % vector part:
+            qt_b(2,1) = (R(3,2) - R(2,3))*s_inv;
+            qt_b(3,1) = (R(1,3) - R(3,1))*s_inv;
+            qt_b(4,1) = (R(2,1) - R(1,2))*s_inv;
+    else
+        % if tr <= 0, find the greatest diagonal element for calculating
+        % the scale factor s and the vector part of the quaternion
+        if ((R(1,1) > R(2,2)) && (R(1,1) > R(3,3)))
+            
+            qt_b(2,1) = 0.5*sqrt(R(1,1) - R(2,2) - R(3,3) + 1);
+            s_inv     = 1/(qt_b(2,1)*4);
 
-    % Define the elements of the rotation matrix
-    r00 = R(1,1);
-    r11 = R(2,2);
-    r22 = R(3,3);
+            qt_b(1,1) = (R(3,2) + R(2,3))*s_inv;
+            qt_b(3,1) = (R(2,1) + R(1,2))*s_inv;
+            qt_b(4,1) = (R(3,1) + R(1,3))*s_inv;
+            
+        elseif (R(2,2) > R(3,3))
+            
+            qt_b(3,1) = 0.5*sqrt(R(2,2) - R(3,3) - R(1,1) + 1);
+            s_inv     = 1/(qt_b(3,1)*4);
 
-    r21 = R(3,2);
-    r12 = R(2,3);
-    r02 = R(1,3);
-
-    r20 = R(3,1);
-    r10 = R(2,1);
-    r01 = R(1,2);
-
-    % Generate the quaternion
-    qw = sqrt(1 +r00 +r11 +r22)/2;
-    qx = (r21 -r12)/(4*qw);
-    qy = (r02 -r20)/(4*qw);
-    qz = (r10 -r01)/(4*qw);
-
-    qt_b = [qw; qx; qy; qz];
+            qt_b(1,1) = (R(1,3) - R(3,1))*s_inv;
+            qt_b(2,1) = (R(2,1) + R(1,2))*s_inv;
+            qt_b(4,1) = (R(3,2) + R(2,3))*s_inv;
+        else
+            qt_b(4,1) = 0.5*sqrt(R(3,3) - R(1,1) - R(2,2) + 1);
+            s_inv     = 1/(qt_b(4,1)*4);
+            
+            qt_b(1,1) = (R(2,1) - R(1,2))*s_inv;
+            qt_b(2,1) = (R(3,1) + R(1,3))*s_inv;
+            qt_b(3,1) = (R(3,2) + R(2,3))*s_inv;
+        end
+    end
 
     % Final state converted into quaternion rapresentation
     q = [H(1:3,4); qt_b];
