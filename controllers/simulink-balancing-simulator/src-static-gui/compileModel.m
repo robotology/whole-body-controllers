272941bcf18828c4306afd816faa9fa21e158add
Gabriele <gabriele.nava@iit.it>
2020-02-19 16:54:07 +0100

    added Simulink balancing simulator

---
+++ controllers/simulink-balancing-simulator/src-static-gui/compileModel.m
@@ -0,0 +1,24 @@
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Compile the Simulink model through Matlab command line.
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+disp('Compiling the Simulink model...')
+
+pause(1.5)
+
+try
+    torqueControlBalancingSim([], [], [], 'compile')
+    torqueControlBalancingSim([], [], [], 'term')
+
+catch ME
+    
+    errorMessages = ME;
+end
+
+clc
+
+disp('Compilation done.')
+
+% warning about Simulink timing
+warning('The model will anyways start with FEW SECONDS OF DELAY after pressing the ''Start Model'' button.')
