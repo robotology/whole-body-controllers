20ba6af98f3891566bbf831f75c8f9b9b179632a
Gabriele <gabriele.nava@iit.it>
2021-08-06 16:41:01 +0200

    reverted checkInputSpikes.m

--- library/matlab-wbc/+wbc/checkInputSpikes.m
+++ library/matlab-wbc/+wbc/checkInputSpikes.m
@@ -1,4 +1,4 @@
-function [noSpikes, res_check_spikes] = checkInputSpikes(u_prev, u, delta_u_max)
+function [noSpikes, res_check_spikes] = checkInputSpikes(u, delta_u_max)
 
     % CHECKINPUTSPIKES checks the (unsigned) difference between two consecutive 
     %                  measurements of the input u, i.e. delta = abs(u(k)-u(k-1))
@@ -6,8 +6,7 @@ function [noSpikes, res_check_spikes] = checkInputSpikes(u_prev, u, delta_u_max)
     %
     % FORMAT: [noSpikes, res_check_spikes] = checkInputSpikes(u, delta_u_max)
     %
-    % INPUT:  - u = [n x 1] input values;
-    %         - u_prev = delayed [n x 1] input values;
+    % INPUT:  - u = [n * 1] input values;
     %         - delta_u_max = user-defined threshold;
     %
     % OUTPUT: - noSpikes = FALSE if delta is greater than delta_u_max;
@@ -20,10 +19,29 @@ function [noSpikes, res_check_spikes] = checkInputSpikes(u_prev, u, delta_u_max)
     %
     % Genoa, Dec 2018
     %
+
+    %% --- Initialization ---
+
+    persistent u_previous
     
-    delta = abs(u - u_prev);
+    if isempty(u_previous)
+        
+        u_previous = u;
+    end
+      
+    delta = abs(u - u_previous);
     
     % u may be a vector
-    res_check_spikes = (delta >= delta_u_max);
-    noSpikes         = double(~any(res_check_spikes));
+    res_check_spikes = delta >= delta_u_max;
+    res_tot          = sum(res_check_spikes);
+    
+    if abs(res_tot) < 0.1
+        
+        noSpikes = 1;
+    else
+        noSpikes = 0;
+    end
+    
+    % now, set u_previous to be u
+    u_previous = u;
 end
